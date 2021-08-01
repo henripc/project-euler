a6d346a7463816da4e1fbd3c30a28543447f7d86
HenriPC <henriquechimetta@outlook.com>
2021-08-01 20:35:17 -0300

    Added solution for problem #56

---
+++ project-euler-56.js
@@ -0,0 +1,22 @@
+// Powerful digit sum
+
+// A googol(10^100) is a massive number: one followed by one - hundred zeros; 100^100 is almost unimaginably large: 
+// one followed by two - hundred zeros.Despite their size, the sum of the digits in each number is only 1.
+
+// Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum ?
+
+// --------------------
+
+const limit = 100;
+let result;
+let maxResult = 0;
+
+for (let i = 2n; i < limit; i++) {
+    for (let j = 1n; j < limit; j++) {
+        result = (i ** j).toString().split("").map(x => Number(x))
+            .reduce((accumulator, currentValue) => accumulator + currentValue);
+        maxResult = result > maxResult ? result : maxResult;
+    }
+}
+
+console.log(maxResult);
