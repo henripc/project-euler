a46004531adc8ccfac79dcd90b23611230596e2e
HenriPC <henriquechimetta@outlook.com>
2021-10-21 12:28:04 -0300

    update solution #3

--- 1-10/project-euler-3.ts
+++ 1-10/project-euler-3.ts
@@ -11,32 +11,31 @@
 //         save i(one time) and divide n by i, increment i by 2 and continue.
 // step 3: If n is a prime number and greater than 2, then n will not become 1 by the 
 //         above two steps.Save n if it is greater than 2.
-function largestPrimeFactor(n: number): number {
+function largestPrimeFactor(num: number): number {
     const primeFactors: Array<number> = [];
     let count = 0;
 
-    while (n % 2 === 0) {
-        n = Math.floor(n / 2);
+    while (num % 2 === 0) {
+        num = Math.floor(num / 2);
         count++;
     }
 
     if (count > 0) primeFactors.push(2);
 
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
         count = 0;
-        while (n % i === 0) {
-            n = Math.floor(n / i);
+        while (num % i === 0) {
+            num = Math.floor(num / i);
             count++;
         }
 
         if (count > 0) primeFactors.push(i);
     }
 
-    if (n > 2) primeFactors.push(n);
+    if (num > 2) primeFactors.push(num);
 
     return Math.max(...primeFactors);
 }
 
 const n = 600_851_475_143;
-const answer = largestPrimeFactor(n);
-console.log(answer);
+console.log(largestPrimeFactor(n));
