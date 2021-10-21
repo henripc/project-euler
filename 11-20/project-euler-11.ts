f9afa709f748800b7fb06d013f39525738120cc6
HenriPC <henriquechimetta@outlook.com>
2021-10-21 12:42:39 -0300

    update solution #11

--- 11-20/project-euler-11.ts
+++ 11-20/project-euler-11.ts
@@ -30,7 +30,7 @@
 
 // --------------------
 
-const grid = `08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+const GRID = `08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
@@ -51,7 +51,7 @@ const grid = `08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48`;
 
-const numbersArray = grid.split(/\s/).map(x => Number(x));
+const numbersArray = GRID.split(/\s/).map(x => Number(x));
 
 // Calculates the max product of 4 numbers in the same row
 // direction: right
@@ -131,5 +131,4 @@ function maxLeftDiagonalProduct(): number {
 
 const firstMax = Math.max(maxHorizontalProduct(), maxVerticalProduct());
 const secondMax = Math.max(maxLeftDiagonalProduct(), maxRightDiagonalProduct());
-const answer = Math.max(firstMax, secondMax);
-console.log(answer);
+console.log(Math.max(firstMax, secondMax));
